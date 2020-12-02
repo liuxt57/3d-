9122109d791f8345d43584a9ce6e082b033c17c8
liuxt <1434791759@qq.com>
2020-12-02 21:28:50 +0800

    1

---
+++ hw08/Assets/elimnation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class elimnation : MonoBehaviour
+{
+    ParticleSystem elimnate;
+    public float size = 4f;
+    void Start()
+    {
+        elimnate = GetComponent<ParticleSystem>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        size = size * 0.99f;
+        var main = elimnate.main;
+        main.startSize = size;
+    }
+
+}
+
+
---
+++ hw08/Assets/midlight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class midlight : MonoBehaviour
+{
+    ParticleSystem midLight;
+    float size = 2000f;
+
+    // Use this for initialization
+    void Start()
+    {
+        midLight = GetComponent<ParticleSystem>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        size = size * 0.99f;
+        var main = midLight.main;
+        main.startSize = size;
+    }
+
+}
